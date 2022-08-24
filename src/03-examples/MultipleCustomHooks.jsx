f50a983d3456eb7a3f94964c067d739b86f49fe8
Duhuhuu <ignisxmizu@gmail.com>
2022-08-24 14:47:26 -0600

    modified:   src/03-examples/MultipleCustomHooks.jsx 	modified:   src/hooks/useFetch.js

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -3,9 +3,10 @@ import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
 
-  const {} = useFetch('https://www.breakingbadapi.com/api/quotes/1');
+  const {data, isLoading, hasError} = useFetch('https://www.breakingbadapi.com/api/quotes/1');
 
-
+  console.log({data, isLoading, hasError});
+  
   return (
 
     <>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export const useFetch = ( url ) => {
 
+    const [estate, setEstate] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+
+    })
+
 
     const getFetch = async () =>{
+
+        setEstate({
+            ...estate,
+            isLoading: true,
+        });
+
         const resp = await fetch(url);
         const data  = await resp.json();
         console.log(data)
 
+        setEstate({
+            data: data,
+            isLoading:false,
+            hasError: null,
+        });
     }
+
     useEffect(() => {
         getFetch();
         
@@ -21,5 +40,9 @@ export const useFetch = ( url ) => {
 
 
 
-  return { };
+  return { 
+    data:      estate.data, 
+    isLoading:  estate.isLoading,
+    hasError:   estate.hasError,
+ };
 }
